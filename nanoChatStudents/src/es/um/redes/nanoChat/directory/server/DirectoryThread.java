6d94e058a34553b28dab6b3cd8be8d52579c5b27
alumno <alumno@lab15>
2019-02-20 12:11:32 +0100

    holi

--- nanoChatStudents/src/es/um/redes/nanoChat/directory/server/DirectoryThread.java
+++ nanoChatStudents/src/es/um/redes/nanoChat/directory/server/DirectoryThread.java
@@ -8,24 +8,23 @@ import java.util.HashMap;
 
 public class DirectoryThread extends Thread {
 
-	//Tamaño máximo del paquete UDP
-	private static final int PACKET_MAX_SIZE = 128;
-	//Estructura para guardar las asociaciones ID_PROTOCOLO -> Dirección del servidor
-	protected HashMap<Integer,InetSocketAddress> servers;
+	int a;
+	private static final int PACKET_MAX_SIZE = 128; //Tamaño máximo del paquete UDP
+	protected HashMap<Integer,InetSocketAddress> servers; //Estructura para guardar las asociaciones ID_PROTOCOLO -> Dirección del servidor
 
-	//Socket de comunicación UDP
-	protected DatagramSocket socket = null;
-	//Probabilidad de descarte del mensaje
-	protected double messageDiscardProbability;
+	protected DatagramSocket socket = null; //Socket de comunicación UDP
+	protected double messageDiscardProbability; //Probabilidad de descarte del mensaje
 
-	public DirectoryThread(String name, int directoryPort,
-			double corruptionProbability)
-			throws SocketException {
+	public DirectoryThread(String name, int directoryPort,double corruptionProbability) throws SocketException {
+		
 		super(name);
-		//TODO Anotar la dirección en la que escucha el servidor de Directorio
- 		//TODO Crear un socket de servidor
+		
+		InetSocketAddress serverAddress = new InetSocketAddress(directoryPort);
+		socket = new DatagramSocket(serverAddress);
+		
 		messageDiscardProbability = corruptionProbability;
-		//Inicialización del mapa
+		
+		
 		servers = new HashMap<Integer,InetSocketAddress>();
 	}
 
@@ -36,20 +35,38 @@ public class DirectoryThread extends Thread {
 		boolean running = true;
 		while (running) {
 
-				//TODO 1) Recibir la solicitud por el socket
-				//TODO 2) Extraer quién es el cliente (su dirección)
+				
+			DatagramPacket pckt = new DatagramPacket(buf, buf.length);
+			// Receive request message
+			try {
+				this.socket.receive(pckt);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("error al recibir");
+			} 
+				
+			
+			InetAddress clienteAddr=pckt.getAddress();
 				// 3) Vemos si el mensaje debe ser descartado por la probabilidad de descarte
 
-				double rand = Math.random();
-				if (rand < messageDiscardProbability) {
-					System.err.println("Directory DISCARDED corrupt request from... ");
-					continue;
-				}
+			double rand = Math.random();
+			if (rand < messageDiscardProbability) {
+				System.err.println("Directory DISCARDED corrupt request from... ");
+				continue;
+			}
 				
 				//TODO (Solo Boletín 2) Devolver una respuesta idéntica en contenido a la solicitud
 				
-				//TODO 4) Analizar y procesar la solicitud (llamada a processRequestFromCLient)
-				//TODO 5) Tratar las excepciones que puedan producirse
+				// 4) Analizar y procesar la solicitud (llamada a processRequestFromCLient)
+			InetSocketAddress ca= (InetSocketAddress) pckt.getSocketAddress();
+			try {
+				processRequestFromClient(buf, ca);
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+				
 		}
 		socket.close();
 	}
@@ -57,16 +74,31 @@ public class DirectoryThread extends Thread {
 	//Método para procesar la solicitud enviada por clientAddr
 	public void processRequestFromClient(byte[] data, InetSocketAddress clientAddr) throws IOException {
 		//TODO 1) Extraemos el tipo de mensaje recibido
+		DatagramPacket pckt = new DatagramPacket(data, data.length, clientAddr);
 		//TODO 2) Procesar el caso de que sea un registro y enviar mediante sendOK
+		sendOK(clientAddr);
 		//TODO 3) Procesar el caso de que sea una consulta
 		//TODO 3.1) Devolver una dirección si existe un servidor (sendServerInfo)
 		//TODO 3.2) Devolver una notificación si no existe un servidor (sendEmpty)
+		
+		
+		
+		// Send response message back to client at address
+		
+		
+		
+		
 	}
 
 	//Método para enviar una respuesta vacía (no hay servidor)
 	private void sendEmpty(InetSocketAddress clientAddr) throws IOException {
 		//TODO Construir respuesta
 		//TODO Enviar respuesta
+		
+		byte[] buf = new byte[PACKET_MAX_SIZE]; // Prepare response message
+		// Send response message back to client at address
+		DatagramPacket pckt = new DatagramPacket(buf, buf.length, clientAddr);
+		socket.send(pckt);
 	}
 
 	//Método para enviar la dirección del servidor al cliente
@@ -80,5 +112,10 @@ public class DirectoryThread extends Thread {
 	private void sendOK(InetSocketAddress clientAddr) throws IOException {
 		//TODO Construir respuesta
 		//TODO Enviar respuesta
+		
+		byte[] buf = new byte[PACKET_MAX_SIZE]; // Prepare response message
+		// Send response message back to client at address
+		DatagramPacket pckt = new DatagramPacket(buf, buf.length, clientAddr);
+		socket.send(pckt);
 	}
 }
