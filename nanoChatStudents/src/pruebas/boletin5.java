abca04c5e38d1d8083fd37f199722ca1e9352131
sara <sara@sara-SATELLITE-L70-A>
2019-03-11 11:34:16 +0100

    Algo que se ha tocado

---
+++ nanoChatStudents/src/pruebas/boletin5.java
@@ -0,0 +1,12 @@
+package pruebas;
+
+import es.um.redes.nanoChat.server.NCServerThread;
+
+public class boletin5 {
+
+	public static void main(String[] args) {
+		
+		NCServerThread server = new NCServerThread();
+	}
+
+}
