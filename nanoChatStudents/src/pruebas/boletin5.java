54aeeeaa5ff726d005a44718d1c3db37e3ff8802
sara <sara@sara-SATELLITE-L70-A>
2019-03-13 13:53:55 +0100

    fdsfdsf

--- nanoChatStudents/src/pruebas/boletin5.java
+++ nanoChatStudents/src/pruebas/boletin5.java
@@ -5,8 +5,6 @@ import es.um.redes.nanoChat.server.NCServerThread;
 public class boletin5 {
 
 	public static void main(String[] args) {
-		
-//		NCServerThread server = new NCServerThread();
 	}
 
 }
