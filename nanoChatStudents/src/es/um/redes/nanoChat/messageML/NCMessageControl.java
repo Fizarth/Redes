dfcf5d5f7502e7476534c81381f4d883134c7a04
sara <sara@sara-SATELLITE-L70-A>
2019-03-21 12:08:30 +0100

    Mensaje Control Done

---
+++ nanoChatStudents/src/es/um/redes/nanoChat/messageML/NCMessageControl.java
@@ -0,0 +1,49 @@
+package es.um.redes.nanoChat.messageML;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * ROOM
+----
+
+<message>
+<operation>operation</operation>
+</message>
+
+Operaciones válidas:
+
+NickOK
+NickDUplicated
+*/
+
+
+public class NCMessageControl extends NCMessage {
+
+	/**
+	 * Creamos un mensaje de tipo Room a partir del código de operación y del nombre
+	 */
+	public NCMessageControl(byte opcode) {
+		this.opcode = opcode;
+
+	}
+
+	@Override
+	//Construimos el mensaje
+	public String toEncodedString() {
+		StringBuffer sb = new StringBuffer();		
+		sb.append("<"+MESSAGE_MARK+">"+END_LINE);
+		sb.append("<"+OPERATION_MARK+">"+opcodeToString(opcode)+"</"+OPERATION_MARK+">"+END_LINE); //Construimos el campo
+		sb.append("</"+MESSAGE_MARK+">"+END_LINE);
+		return sb.toString();
+	}
+
+	//Parseamos el mensaje
+	public static NCMessageControl readFromString(byte code) {		
+		return new NCMessageControl(code);
+	}
+
+
+}
+
+
+
