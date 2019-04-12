6b69b6d5594c4c89f4ad47e5ababb4975cea33b1
tururu22 <blancagbeltran@gmail.com>
2019-04-12 11:39:03 +0200

    añadidos resto de mensages

---
+++ nanoChatStudents/src/es/um/redes/nanoChat/messageML/NCMessageInfoRoom.java
@@ -0,0 +1,30 @@
+package es.um.redes.nanoChat.messageML;
+
+/*
+ * INFO ROOM
+----
+
+<message>
+      <operation>opCode</operation>
+      <room>room1</room>
+      <name>NombreRoom2</name>      
+      <numUser>2</numUser>
+      <nick>nick1</nick>
+      <nick>nick2</nick>
+</message>
+
+
+Operaciones válidas:
+
+"InfoRoomRequest"
+*/
+public class NCMessageInfoRoom extends NCMessage{
+
+	//-----TODO modificar
+	@Override
+	protected String toEncodedString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ nanoChatStudents/src/es/um/redes/nanoChat/messageML/NCMessageRoomsInfo.java
@@ -0,0 +1,40 @@
+package es.um.redes.nanoChat.messageML;
+/*
+ * ROOMS INFO
+----
+
+<message>
+      <operation>opCode</operation>
+      <room>
+            <name>NombreRoom1</name>      
+            <numUser>5</numUser>
+      </room>
+      <room>
+            <name>NombreRoom2</name>      
+            <numUser>1</numUser>
+      </room>
+      <room>
+            <name>NombreRoom3</name>      
+            <numUser>10</numUser>
+      </room>
+        ….
+</message>
+
+
+
+Operaciones válidas:
+
+"ListRoom"
+*/
+
+public class NCMessageRoomsInfo extends NCMessage{
+
+	
+	//-----TODO modificar
+	@Override
+	protected String toEncodedString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
