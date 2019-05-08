cd9f7653a7a0a663d2f78239ab2342602c131398
tururu22 <blancagbeltran@gmail.com>
2019-05-08 11:43:39 +0200

    infomensahe

---
+++ nanoChatStudents/src/es/um/redes/nanoChat/server/roomManager/InfoMensaje.java
@@ -0,0 +1,14 @@
+package es.um.redes.nanoChat.server.roomManager;
+
+import java.util.ArrayList;
+
+public class InfoMensaje {
+	
+	public String usuario;
+	public String texto;
+	
+	public InfoMensaje(String usuario, String text) {
+		this.usuario=usuario;
+		this.texto=text;
+	}
+}
