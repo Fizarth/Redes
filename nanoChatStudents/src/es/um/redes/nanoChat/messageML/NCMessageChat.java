78b983089cf8f003588aed93d3f1d22cfef47610
sara <sara@sara-SATELLITE-L70-A>
2019-05-08 11:24:03 +0200

    message chat

--- nanoChatStudents/src/es/um/redes/nanoChat/messageML/NCMessageChat.java
+++ nanoChatStudents/src/es/um/redes/nanoChat/messageML/NCMessageChat.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 
 <message>
 	<operation> opCode</operation>
-	<usur> usuario </usur>
+	<user> usuario </user>
 	<mensaje>Mensaje de Texto </mensaje>
 </message>
 
@@ -21,10 +21,16 @@ Operaciones válidas:
 public class NCMessageChat extends NCMessage{
 	
 	private String mensaje;
+	private String user;
 	
 	//Constantes asociadas a las marcas específicas de este tipo de mensaje
 	private static final String RE_NAME = "<mensaje>(.*?)</mensaje>";
 	private static final String NAME_MARK = "mensaje";
+	
+	private static final String RE_USER = "<user>(.*?)</user>";
+	private static final String USER_MARK = "user";
+	
+	private static final String patron  = "<([^message]\\w+?)>(.*?)</\\1>";
 
 	/**
 	 * Creamos un mensaje de tipo Room a partir del código de operación y del nombre
@@ -32,6 +38,7 @@ public class NCMessageChat extends NCMessage{
 	public NCMessageChat(byte opcode, String usuario, String msg) {
 		this.opcode = opcode;
 		this.mensaje = msg;
+		this.user = usuario;
 	}
 
 	@Override
@@ -41,6 +48,7 @@ public class NCMessageChat extends NCMessage{
 		
 		sb.append("<"+MESSAGE_MARK+">"+END_LINE);
 		sb.append("<"+OPERATION_MARK+">"+opcodeToString(opcode)+"</"+OPERATION_MARK+">"+END_LINE); //Construimos el campo
+		sb.append("<"+USER_MARK+">"+user+"</"+USER_MARK+">"+END_LINE);
 		sb.append("<"+NAME_MARK+">"+mensaje+"</"+NAME_MARK+">"+END_LINE);
 		sb.append("</"+MESSAGE_MARK+">"+END_LINE);
 
@@ -52,24 +60,48 @@ public class NCMessageChat extends NCMessage{
 	//Parseamos el mensaje contenido en message con el fin de obtener los distintos campos
 	public static NCMessageChat readFromString(byte code, String message) {
 		String found_name = null;
-
+		String found_user = null;
 		// Tienen que estar los campos porque el mensaje es de tipo RoomMessage
-		Pattern pat_name = Pattern.compile(RE_NAME);
-		Matcher mat_name = pat_name.matcher(message);
-		if (mat_name.find()) {
-			// Name found
-			found_name = mat_name.group(1);
-		} else {
+		
+	
+		
+		Pattern pat = Pattern.compile(patron);
+		Matcher mat_name = pat.matcher(message);
+		
+		
+		while (mat_name.find()) {
+			switch (mat_name.group(1)) {
+			case USER_MARK:
+				found_user= mat_name.group(2);
+				break;
+
+			case NAME_MARK:
+				found_name = mat_name.group(2);
+				break;
+			default:
+				break;
+			}
+		}
+			
+		if(found_name!=null || found_user != null){
+			return new NCMessageChat(code, found_user,found_name);
+		}	
+		else {		
 			System.out.println("Error en MessageChat: no se ha encontrado parametro.");
-			return null;
+			return null;	
 		}
 		
-		return new NCMessageChat(code, found_name);
+		
 	}
+	
 
 
 	//Devolvemos el nombre contenido en el mensaje
 	public String getName() {
 		return mensaje;
+	}
+	
+	public String getUser() {
+		return user;
 	}
 }
